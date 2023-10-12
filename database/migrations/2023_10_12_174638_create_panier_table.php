79fe4bed31508ece0b2f1ed0e550831bc7cbc518
elboudna <mehdi_boudnaoui@hotmail.fr>
2023-10-12 13:54:31 -0400

    ajout des table panier et panier-produit

---
+++ database/migrations/2023_10_12_174638_create_panier_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('panier', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('utilisateur_id')->nullable(); // Si le panier est associé à un utilisateur
+            $table->timestamps();
+
+            // Ajoutez d'autres colonnes au besoin, par exemple, pour stocker le total du panier.
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('panier');
+    }
+};
---
+++ database/migrations/2023_10_12_174646_create_panier_produit_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('panier_produit', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('panier_id');
+            $table->unsignedBigInteger('produit_id');
+            $table->integer('quantite');
+            $table->string('taille'); // Ajoutez la colonne pour la taille ici
+
+            $table->timestamps();
+
+            $table->foreign('panier_id')->references('id')->on('panier');
+            $table->foreign('produit_id')->references('id')->on('produit');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('panier_produit');
+    }
+};
