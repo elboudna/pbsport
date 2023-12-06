0e8c6275a237fdb48468c472e3c344afc0ef6a71
elboudna <mehdi_boudnaoui@hotmail.fr>
2023-12-05 20:04:56 -0500

    ajout des seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,11 +14,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        // run rolestable and typestable seeder
+        $this->call([
+            RolesTableSeeder::class,
+            TypesTableSeeder::class,
+        ]);
     }
 }
