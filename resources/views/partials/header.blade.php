a5c0bd5d4080eab70bc683e7388b5721252b5f66
elboudna <mehdi_boudnaoui@hotmail.fr>
2023-12-25 21:17:49 -0500

    ajout de la responsivité des ecrans mobiles

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,7 +1,7 @@
 <!-- resources/views/partials/header.blade.php -->
 
 <header class="bgvert">
-    <div>
+    <div id="desktop-nav">
         <div>
             <p>514-865-1102</p>
         </div>
@@ -9,7 +9,6 @@
             <p>info@pbsportplus.com</p>
         </div>
         <div>
-
             <div class="user-menu">
                 <p>
                     @auth
@@ -22,7 +21,7 @@
                 <div class="menu-options">
                     @auth
                     @if(auth()->user()->role_id == 3)
-                        <a href="{{ route('admin.gestion') }}">Gestion</a>
+                    <a href="{{ route('admin.gestion') }}">Gestion</a>
                     @endif <a href="{{ route('profil') }}">Profil</a>
                     <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
@@ -39,7 +38,55 @@
                 <img src="/icone/panier.png" alt="panier">
             </a>
         </div>
+    </div>
 
+    <div id="mobile-nav">
+        <div id="burger-menu">
+            <div id="burger-menu-content">
+                <div class="user-menu">
+                    <div>
+                        @auth
+                        <p class="compte-mobile-head">{{ Auth::user()->prenom }}</p>
+                        @else
+                        <p class="compte-mobile-head">Compte</p>
+                        <a href="{{ route('showlogin') }}">Se connecter</a>
+                        <a href="{{ route('showregister') }}">Nouveau compte</a>
+                        @endauth
+                    </div>
+                    @auth
+                    <div>
+                        @if(auth()->user()->role_id == 3)
+                        <a href="{{ route('admin.gestion') }}">Gestion</a>
+                        @endif
+                        <a href="{{ route('profil') }}">Profil</a>
+                        <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Se deconnecter</a>
+                    </div>
+                    @endauth
+
+                    <div id="nav-mobile">
+                        <nav>
+                            <a href="/">Accueil</a>
+                            <a href="{{ route('evenements') }}">Evenements</a>
+                            <a href="{{ route('magasin') }}">Produits</a>
+                            <a href="#">Blog</a>
+                            <a href="#">Contact</a>
+                        </nav>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        <div id="burger-icon">&#9776;</div>
+        <div id="logo-nav-mobile">
+            <a href="/">
+                <img src="/image/pbs.jpg" alt="logo">
+            </a>
+        </div>
+        <div class="panier-icone">
+            <a href="{{ route('panier') }}">
+                <img src="/icone/panier.png" alt="panier">
+            </a>
+        </div>
     </div>
 </header>
 
@@ -58,3 +105,22 @@
         <a href="#">Contact</a>
     </nav>
 </section>
+
+<script>
+    document.addEventListener("DOMContentLoaded", function() {
+        var burgerIcon = document.getElementById("burger-icon");
+        var burgerMenu = document.getElementById("burger-menu");
+
+        burgerIcon.addEventListener("click", function() {
+            if (burgerMenu.style.display == "block") {
+                burgerMenu.style.display = "none";
+                burgerIcon.style.zIndex = "1";
+                burgerIcon.style.color = "black";
+            } else {
+                burgerMenu.style.display = "block";
+                burgerIcon.style.color = "white";
+                burgerIcon.style.zIndex = "1000";
+            }
+        });
+    });
+</script>
