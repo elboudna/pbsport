f38b9881b61744ac2e9f5ca43428ff7137b3fb59
elboudna <mehdi_boudnaoui@hotmail.fr>
2023-12-30 15:26:49 -0500

    mise a jour de  la modification de evenement

--- app/Http/Controllers/EvenementController.php
+++ app/Http/Controllers/EvenementController.php
@@ -65,14 +65,12 @@ class EvenementController extends Controller
     {
         $evenement = Evenement::findOrFail($id);
 
-        $evenement->type = $request->type;
-        $evenement->nom = $request->nom;
         $evenement->description = $request->description;
         $evenement->date_debut = $request->date_debut;
         $evenement->heure = $request->heure;
         $evenement->lieu = $request->lieu;
         $evenement->adresse = $request->adresse;
-        $evenement->nbr_joueur = $request->nombre_joueurs;
+        $evenement->nbr_joueur = $request->nbr_joueur;
         $evenement->prix = $request->prix;
         $evenement->categorie = $request->categorie;
         $evenement->niveau = $request->niveau;
@@ -80,16 +78,23 @@ class EvenementController extends Controller
         $evenement->email = $request->email;
         $evenement->lien = $request->lien;
 
+        // validate the image and size and format
+        $request->validate([
+            'image' => 'image|mimes:jpeg,png,jpg,gif,svg|max:4048',
+        ]);
+
 
         if ($request->hasFile('image')) {
             $file = $request->file('image');
-            $extension = $file->getClientOriginalExtension(); // getting image extension
+            $extension = $file->getClientOriginalExtension();
             $filename = time() . '.' . $extension;
             $file->move('storage/evenement_images/', $filename);
+    
+            // Set the image attribute on the model
             $evenement->image = $filename;
-        } else {
-            return $request;
-            $evenement->image = '';
+        }
+        else {
+            $evenement->image = ''; // or set it to null or any default value
         }
 
         $evenement->save();
