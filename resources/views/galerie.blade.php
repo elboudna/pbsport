80b0b221912f50675724637dcf8ee5befbb0a7ef
elboudna <mehdi_boudnaoui@hotmail.fr>
2024-01-11 15:08:00 -0500

    modification de la page Galerie

---
+++ resources/views/galerie.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+
+<section id="section-coachs">
+
+    <h2>Notre galerie</h2>
+    
+    <div class="coach">
+              
+    </div>
+    
+</section>
+@endsection
