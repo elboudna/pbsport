dadfd16d8f2d0b96d19df8d1c4fafb4ac1b9b6d7
elboudna <mehdi_boudnaoui@hotmail.fr>
2024-01-11 15:07:29 -0500

    modification de la page Accueil

--- app/Http/Controllers/AccueilController.php
+++ app/Http/Controllers/AccueilController.php
@@ -2,23 +2,29 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 
 class AccueilController extends Controller
 {
     public function index()
     {
+        app()->setLocale('fr');
+
         // select 4 random produit de la table produit
         $produits = \App\Models\Produit::inRandomOrder()->take(4)->get();
 
         //select all the banner from the table banniere
         $bannieres = \App\Models\Banniere::all();
 
+        //select 4 random image from the table galerie
+        $galeries = \App\Models\Galerie::inRandomOrder()->take(4)->get();
+
+        // select all event order by date_debut from the table evenement, and show the first
+        $evenements = \App\Models\Evenement::orderBy('date_debut', 'asc')->take(2)->get();
         return view('accueil', [
             'produits' => $produits,
-            'bannieres' => $bannieres
+            'bannieres' => $bannieres,
+            'galeries' => $galeries,
+            'evenements' => $evenements
         ]);
-
-
     }
 }
