fb47a6727b7fe35ba81011f5086561d567009ab0
elboudna <mehdi_boudnaoui@hotmail.fr>
2024-01-11 15:08:39 -0500

    modification de la page Coach

--- resources/views/coachs.blade.php
+++ resources/views/coachs.blade.php
@@ -6,25 +6,9 @@
 
     <h2>Nos Coachs</h2>
     
-    @foreach($evenements as $evenement)
     <div class="coach">
-            <div class="coach-div">
-                <div class="coach-img">
-                    <img src="{{ asset('storage/evenement_images/' . $evenement->image) }}" alt="{{ $evenement->nom }}">
-                </div>
-                <div class="coach-flex">
-                    <div>
-                        <h3>{{ $evenement->nom }}</h3>
-                        <p>Date de début : {{ \Carbon\Carbon::parse($evenement->date_debut)->isoFormat('D MMMM YYYY', 'Do MMMM YYYY') }}</p>
-                        <p>{{ $evenement->description }}</p>
-                    </div>
-                    <div class="btn-ensavoirplus">
-                        <a href="{{ route('evenement.show', $evenement->id) }}">En savoir en plus</a>
-                    </div>
-                </div>
-            </div>    
+              
     </div>
-    @endforeach
     
 </section>
 @endsection
