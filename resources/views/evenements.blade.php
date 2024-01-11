d479b9725eef044e665c6de055c496eede1d74a5
elboudna <mehdi_boudnaoui@hotmail.fr>
2024-01-11 15:07:17 -0500

    modification de la page Evenement

--- resources/views/evenements.blade.php
+++ resources/views/evenements.blade.php
@@ -7,26 +7,23 @@
 <section id="section-evenements">
 
     <h2>Liste des événements</h2>
-    
-    @foreach($evenements as $evenement)
+
     <div class="event">
-            <div class="evenement-div">
-                <div class="evenement-img">
-                    <img src="{{ asset('storage/evenement_images/' . $evenement->image) }}" alt="{{ $evenement->nom }}">
+    @foreach($evenements as $evenement)
+        <div class="evenement-div">
+            <div class="evenement-img">
+                <img src="{{ asset('storage/evenement_images/' . $evenement->image) }}" alt="{{ $evenement->nom }}">
+            </div>
+            <div class="evenement-flex">
+                <h3>{{ $evenement->nom }}</h3>
+                <p>Date de début : {{ \Carbon\Carbon::parse($evenement->date_debut)->isoFormat('D MMMM YYYY', 'Do MMMM YYYY') }}</p>
+                <div class="btn-ensavoirplus-event">
+                    <a href="{{ route('evenement.show', $evenement->id) }}">En savoir en plus</a>
                 </div>
-                <div class="evenement-flex">
-                    <div>
-                        <h3>{{ $evenement->nom }}</h3>
-                        <p>Date de début : {{ \Carbon\Carbon::parse($evenement->date_debut)->isoFormat('D MMMM YYYY', 'Do MMMM YYYY') }}</p>
-                        <p>{{ $evenement->description }}</p>
-                    </div>
-                    <div class="btn-ensavoirplus">
-                        <a href="{{ route('evenement.show', $evenement->id) }}">En savoir en plus</a>
-                    </div>
-                </div>
-            </div>    
+            </div>
+        </div>
+        @endforeach
     </div>
-    @endforeach
-    
+
 </section>
 @endsection
