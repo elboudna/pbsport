a7975125c18b061ce2c9d49c52deb0afaafb0abb
elboudna <mehdi_boudnaoui@hotmail.fr>
2024-01-15 01:37:21 -0500

    ajout de la page erreur 404

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+
+<section>
+
+    <h1>404 Not Found</h1>
+    <p>Sorry, the page you are looking for could not be found.</p>
+    <!-- Add any additional content or styling as needed -->
+</section>
+
+
+@endsection
