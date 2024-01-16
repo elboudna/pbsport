c1b7d9b804907dec559268cd2e32710c54e3b7d7
elboudna <mehdi_boudnaoui@hotmail.fr>
2024-01-16 14:19:42 -0500

    mise a jour de la page magasin pour afficher message en cas de produit non existant

--- resources/views/magasin.blade.php
+++ resources/views/magasin.blade.php
@@ -8,52 +8,15 @@
     <h2>Produits</h2>
 </div>
 
-<section class="margin-bottom margin-top" id="magasin">
-    <div class="flex-row">
-        <div id="magasin-gauche">
-            <div>
-                <h3>Categorie</h3>
-                <p>Raquettes</p>
-                <p>Balles</p>
-                <p>Vetements</p>
-                <p>Accessoires</p>
-            </div>
-            <div>
-                <h3>Couleur</h3>
-                <p>Noir</p>
-                <p>Rouge</p>
-                <p>Vert</p>
-                <p>Blanc</p>
-            </div>
-        </div>
-        <div id="magasin-droite">
-
-            <div id="tri-magasin">
-                <select name="tri">
-                    <option value="">Trier par</option>
-                    <option value="">Dernier arrivage</option>
-                    <option value="">Prix croissant</option>
-                    <option value="">Prix décroissant</option>
-                </select>
-            </div>
-
-            <div id="produits-magasin">
-                @foreach ($produits as $produit)
-                <a href="{{ route('produit.show', ['id' => $produit->id]) }}">
-                    <div class="produit-magasin">
-                        @if ($produit->images->isNotEmpty())
-                        <img src="{{ asset('storage/produit_images/' . $produit->images->first()->chemin) }}" alt="{{ $produit->nom }}">
-                        @else
-                        <img src="{{ asset('placeholder.jpg') }}" alt="{{ $produit->nom }}">
-                        @endif
-                        <p class="nom-produit-accueil">{{ $produit->nom }}</p>
-                        <p class="prix-produit-accueil vert">{{ $produit->prix }}$</p>
-                    </div>
-                </a>
-                @endforeach
-            </div>
-
-        </div>
+<section id="magasin">
+    <span>
+        <p>Aucun produit trouvé!</p>
+    </span>
+    <p>Revenez plus tard.</p>
+    <!-- retour à la page d'accueil -->
+    <div class="lien-retour">
+        <a href="{{ route('accueil') }}">Retour à la page d'accueil</a>
     </div>
+
 </section>
 @endsection
