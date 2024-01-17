c11499a3b818a2f9a448365697b31c24d2881d3b
elboudna <mehdi_boudnaoui@hotmail.fr>
2024-01-16 20:03:41 -0500

    ajout d image dans la page 404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -5,7 +5,11 @@
     <h2>Page introuvable</h2>
 </div>
 <section class="section-error">
-    <span> <p>404!</p> </span>
+    <span> 
+        <img src="{{ asset('icone/raquetteG.png') }}" alt="">
+        <p>404!</p> 
+        <img src="{{ asset('icone/raquetteD.png') }}" alt="">
+    </span>
     <p>La page que vous cherchez n'existe pas.</p>
     <!-- retour à la page d'accueil -->
     <div class="lien-retour">
