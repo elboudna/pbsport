c1d9ae9297c1012f0c26f260746f67ab6de09659
elboudna <mehdi_boudnaoui@hotmail.fr>
2024-01-18 10:44:39 -0500

    creation de la page contact

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -6,4 +6,44 @@
     <h2>Contact</h2>
 </div>
 
+<section id="contact" class="form-section">
+
+    <div class="form-pbsport">
+
+        <form action="{{ route('contact.send') }}" method="POST">
+            @csrf
+            <div class="form-group">
+                <label for="nom">Nom</label>
+                <input type="text" class="form-control" id="nom" name="nom" required>
+            </div>
+
+            <div class="form-group">
+                <label for="prenom">Prenom</label>
+                <input type="text" class="form-control" id="prenom" name="prenom" required>
+            </div>
+
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input type="text" class="form-control" id="email" name="mail" required>
+            </div>
+
+            <div class="form-group">
+                <label for="telephone">Telephone</label>
+                <input type="text" class="form-control" id="telephone" name="telephone" required>
+            </div>
+
+            <div class="form-group">
+                <label for="message">Message</label>
+                <textarea class="form-control" id="message" name="message" required></textarea>
+            </div>
+
+            <div class="button-center">
+                <button type="submit" class="bgvert">Envoyer</button>
+            </div>
+
+        </form>
+    </div>
+
+</section>
+
 @endsection
