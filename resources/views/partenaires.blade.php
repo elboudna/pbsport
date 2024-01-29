f107ab3baf73566aa034cf5e4f2853d6887a119f
elboudna <mehdi_boudnaoui@hotmail.fr>
2024-01-29 14:49:50 -0500

    finalisation de la page partenaires tout ecran

--- resources/views/partenaires.blade.php
+++ resources/views/partenaires.blade.php
@@ -9,15 +9,15 @@
 <section id="section-partenaires">
     <div id="partenaire-majeur">
         <h2>Partenaire majeur</h2>
-        <img src="{{ asset('image/partenaires/tennis13.png') }}" alt="">
+        <a target="_blank" href="https://tennis13.com/en/pickleball-2/"><img src="{{ asset('image/partenaires/tennis13.png') }}" alt=""></a>
     </div>
 
     <div id="partenaires">
         <h2>Autres partenaires</h2>
         <div>
-            <img src="{{ asset('image/partenaires/hudef.png') }}" alt="">
-            <img src="{{ asset('image/partenaires/pbsport.png') }}" alt="">
-            <img src="{{ asset('image/partenaires/onix.png') }}" alt="">
+            <a target="_blank" href="https://hudefsport.com/"><img src="{{ asset('image/partenaires/hudef.png') }}" alt=""></a>
+            <a target="_blank" href="https://pbsport.ca/"><img src="{{ asset('image/partenaires/pbsport.png') }}" alt=""></a>
+            <a target="_blank" href="https://www.onixpickleball.com/"><img src="{{ asset('image/partenaires/onix.png') }}" alt=""></a>
         </div>
     </div>
 </section>
