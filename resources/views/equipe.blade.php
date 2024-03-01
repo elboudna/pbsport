5374de2e90a7b436e1e9300a6fdf8322c28761f2
elboudna <mehdi_boudnaoui@hotmail.fr>
2024-03-01 12:59:16 -0500

    creation page equipe

---
+++ resources/views/equipe.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="bande">
+    <h2>Equipe</h2>
+</div>
+
+<section id="section-equipe">
+
+    <div class="team">
+        <div class="team-member">
+            <img src="photo1.jpg" alt="Membre 1">
+            <h3>Nom Prénom</h3>
+            <p>Poste</p>
+        </div>
+        <div class="team-member">
+            <img src="photo2.jpg" alt="Membre 2">
+            <h3>Nom Prénom</h3>
+            <p>Poste</p>
+        </div>
+        <div class="team-member">
+            <img src="photo2.jpg" alt="Membre 2">
+            <h3>Nom Prénom</h3>
+            <p>Poste</p>
+        </div>
+        <div class="team-member">
+            <img src="photo2.jpg" alt="Membre 2">
+            <h3>Nom Prénom</h3>
+            <p>Poste</p>
+        </div>
+        <div class="team-member">
+            <img src="photo2.jpg" alt="Membre 2">
+            <h3>Nom Prénom</h3>
+            <p>Poste</p>
+        </div>
+        <!-- Ajoutez d'autres membres de l'équipe ici -->
+    </div>
+
+</section>
+
+@endsection
