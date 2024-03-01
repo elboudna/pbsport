604aa8f06788eaa0eeae6f12d6c1337e18806528
elboudna <mehdi_boudnaoui@hotmail.fr>
2024-03-01 12:59:43 -0500

    creation page equipe

---
+++ app/Http/Controllers/EquipeController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class EquipeController extends Controller
+{
+    //
+
+    public function index()
+    {
+        return view('equipe');
+    }
+}
