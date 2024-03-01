32ff88c9338465b41fe655ef4bfbb6830204159a
elboudna <mehdi_boudnaoui@hotmail.fr>
2024-03-01 15:02:30 -0500

    mise a jour

--- database/migrations/2023_12_15_191629_create_galeries_table.php
+++ database/migrations/2023_12_15_191629_create_galeries_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
     {
         Schema::create('galeries', function (Blueprint $table) {
             $table->id();
-            $table->string('nom');
             $table->string('image');
             $table->timestamps();
         });
