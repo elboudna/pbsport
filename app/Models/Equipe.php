95edaaa773f3924a917ebf11b655e41ce0bc7ef3
elboudna <mehdi_boudnaoui@hotmail.fr>
2024-03-06 11:49:48 -0500

    MAJ page equipe

--- app/Models/Equipe.php
+++ app/Models/Equipe.php
@@ -7,8 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Equipe extends Model
 {
 
+    //database name is equipe
+    protected $table = 'equipe'; // Make sure it matches your actual table name
+    
     protected $fillable = [
-        'nom', 'prenom', 'poste'
+        'nom', 'prenom', 'poste', 'image'
         // Add other fields as needed
     ];
 }
