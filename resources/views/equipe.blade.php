2c93ae8944d6f2ddba6a370a5420d2218ea1841f
elboudna <mehdi_boudnaoui@hotmail.fr>
2024-03-07 19:40:05 -0500

    ajout de titre et sous-titre

--- resources/views/equipe.blade.php
+++ resources/views/equipe.blade.php
@@ -8,8 +8,10 @@
 
 <section id="section-equipe">
 
-    <div class="team">
+    <h1 class="titre-page">Notre équipe</h1>
+    <h4 class="sous-titre-page">PBSport plus dispose d’une équipe polyvalente possédant une connaissance et une experience dans le monde Pickleball.</h4>
 
+    <div class="team">
         @foreach($membres as $membre)
         <div class="team-member">
             <img src="{{ asset('storage/equipe_images/' . $membre->image) }}" alt="$membre->nom">
